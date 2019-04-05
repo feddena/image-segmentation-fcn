ea6cd2d1939e8522b719b71938cdbdb661e37e92
fedor.denisov <fedor.denisov@corp.mail.ru>
2019-04-05 15:24:58 +0300

    resize with padding

--- source_eggs_and_pan.py
+++ source_eggs_and_pan.py
@@ -26,6 +26,52 @@ def build_file_list(path_to_folder):
         file_list.append((path_to_folder + "/images/" + image_name, path_to_folder + "/masks/" + image_name))
     return file_list
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+
+def resize_with_padding(image, size):
+    h, w = image.shape[:2]
+    if h/size[0] == w/size[1]:
+        return image_resize(image, height=size[0])
+
+    if h/size[0] > w/size[1]:
+        resized = image_resize(image, height=size[0])
+    else:
+        resized = image_resize(image, width=size[1])
+
+    h_res, w_res = resized.shape[:2]
+
+    return cv2.copyMakeBorder(resized, 0, 0, w - w_res, h - h_res, cv2.BORDER_CONSTANT, value=BLACK)
+
 
 class EggsAndPansSource:
     def __init__(self):
@@ -77,8 +123,10 @@ class EggsAndPansSource:
                     image_file = f[0]
                     label_file = f[1]
 
-                    image = cv2.resize(cv2.imread(image_file), self.image_size)
-                    label = cv2.resize(cv2.imread(label_file), self.image_size)
+                    image = cv2.resize_with_padding(cv2.imread(image_file), self.image_size)
+                    label = cv2.resize_with_padding(cv2.imread(label_file), self.image_size)
+
+                    image.shape()
 
                     label_bg = np.zeros([image.shape[0], image.shape[1]], dtype=bool)
                     label_list = []
